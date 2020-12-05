d6aad225b17a5d8ba540292c35ae52aa5fa30640
016270 <yanglong@htsc.com>
2020-12-05 14:30:15 +0800

    add vision

---
+++ CoreMLDemo/VisionBasicViewController.swift
@@ -0,0 +1,17 @@
+//
+//  VisionBasicViewController.swift
+//  CoreMLDemo
+//
+//  Created by Yang Long on 2020/12/5.
+//
+
+import UIKit
+import Vision
+
+class VisionBasicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
